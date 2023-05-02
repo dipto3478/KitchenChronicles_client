17a372cb4662d5beea2ff1c0ad989a6b44ab0ec2
dipto3478 <kitkatboy3478@gmail.com>
2023-05-02 18:03:42 +0600

    login done

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,9 +1,31 @@
 import React from "react";
+import { Button, Form } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const Login = () => {
   return (
-    <div>
-      <h3>this is login</h3>
+    <div
+      style={{ height: "calc(100vh - 62px)" }}
+      className="container  d-flex align-items-center justify-content-center"
+    >
+      <Form className="w-25">
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+
+        <p>
+          Do not have an Accounts? <Link to="/register">Please Register</Link>
+        </p>
+        <Button className="w-100" variant="primary" type="submit">
+          Login
+        </Button>
+      </Form>
     </div>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -9,6 +9,7 @@ import Register from "./components/Register/Register.jsx";
 import Blog from "./components/Blog/Blog.jsx";
 import ChefDetails from "./components/ChefDetails/ChefDetails.jsx";
 import { ToastContainer } from "react-toastify";
+import AuthProvider from "./components/AuthProvider/AuthProvider.jsx";
 
 const router = createBrowserRouter([
   {
@@ -44,7 +45,9 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
-    <ToastContainer />
+    <AuthProvider>
+      <RouterProvider router={router} />
+      <ToastContainer />
+    </AuthProvider>
   </React.StrictMode>
 );

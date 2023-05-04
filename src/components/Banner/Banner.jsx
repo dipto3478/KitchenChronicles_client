88c7aa3c7a0fa6a35f5206f6938c63237c4386c6
dipto3478 <kitkatboy3478@gmail.com>
2023-05-04 09:58:48 +0600

    banner animation

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,11 +1,27 @@
 import React from "react";
+import { TypeAnimation } from "react-type-animation";
 import "./Banner.css";
 
 const Banner = () => {
   return (
     <section className="banner-container">
       <article className="text-light text-center">
-        <h1 className="display-1 fw-bolder">Where Recipes Come to Life</h1>
+        <h1 className="display-3 fw-bolder">
+          <TypeAnimation
+            sequence={[
+              " Where ",
+              1000,
+              " Where Recipes ",
+              1000,
+              " Where Recipes Come ",
+              1000,
+              " Where Recipes Come to Life",
+              1000,
+            ]}
+            speed={75}
+            repeat={Infinity}
+          />
+        </h1>
         <p className="fs-5">
           A vibrant and colorful image of various ingredients and kitchen tools
           with the website's name and slogan overlaid in a fun and playful font.
